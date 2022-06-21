14b761ea02904fdc53e122229203392a18ff221c
oloapinivad <p.davini@isac.cnr.it>
2022-06-21 17:28:43 +0200

    expanding the structure to postproc

--- CDS_retriever.py
+++ CDS_retriever.py
@@ -1,6 +1,29 @@
 import cdsapi
 import os
+import glob
+from cdo import Cdo
+cdo = Cdo()
 
+# check with cdo is the file is complete (approximately correct)
+def is_file_complete(filename, minimum_steps) : 
+    try : 
+        out = cdo.ntime(input=filename, options = '-s')
+        # this is an hack due to warning being reported by cdo into the output!
+        for n in out : 
+            if (len(n) <= 5) :
+                nt = n  
+    except : 
+        print(filename + ' is missing')
+        nt = 0 
+    print(nt)
+    if (int(nt) < minimum_steps) :
+        print('Need to retrieve ' + filename)
+        retrieve = True
+    else :
+        print(filename + ' is complete! Going to next one...')
+        retrieve = False
+    return retrieve
+    
  # big function for retrieval
 def year_retrieve(var, freq, year, grid, levelout, outdir) : 
 
@@ -8,46 +31,48 @@ def year_retrieve(var, freq, year, grid, levelout, outdir) :
     level, level_kind = define_level(levelout)
     kind = 'reanalysis-era5-' + level_kind 
 
-    product_type, day, time, time_kind = define_time(freq)
+    product_type, day, time, time_kind, minimum_steps = define_time(freq)
     kind = kind + time_kind
 
     months = [str(i).zfill(2) for i in range(1,12+1)]
 
-    filename = 'ERA5_' + var + '_' + freq + '_' + grid + '_' + levelout + '_' + year + '.grib'
+    filename =  create_filename(var, freq, grid, levelout, year) + '.grib'
     outfile = os.path.join(outdir, filename)
 
-
-    # special feature for preliminary back extension
-    if int(year) < 1959 :
-        kind = kind + 'preliminary-back-extension'
-
-     # check what I am making up
-    biglist = ['kind', 'product_type', 'var', 'year', 'freq', 'months', 'day', 'time', 'levelout', 'level', 'grid', 'outfile']
-    for test in biglist : 
-        if isinstance(locals()[test], list):
-            print(test + ': ' + ' '.join(locals()[test]))
-        else : 
-            print(test + ': ' + locals()[test])
-
-    # get right grid for the API call
-    gridapi = grid.split('x')[0]
+    run_year = is_file_complete(outfile, minimum_steps) 
+    if run_year : 
     
-    # run the API
-    c = cdsapi.Client()
-    c.retrieve(
-        kind,
-            {
-            'product_type': product_type,
-            'format': 'grib',
-            'variable': var,
-            'pressure_level': level,
-            'year': year,
-            'month': months,
-            'day': day,
-            'time': time,
-            'grid': [ gridapi, gridapi ]
-        },
-    outfile)
+        # special feature for preliminary back extension
+        if int(year) < 1959 :
+            kind = kind + 'preliminary-back-extension'
+
+        # check what I am making up
+        biglist = ['kind', 'product_type', 'var', 'year', 'freq', 'months', 'day', 'time', 'levelout', 'level', 'grid', 'outfile']
+        for test in biglist : 
+            if isinstance(locals()[test], list):
+                print(test + ': ' + ' '.join(locals()[test]))
+            else : 
+                print(test + ': ' + locals()[test])
+
+            # get right grid for the API call
+            gridapi = grid.split('x')[0]
+
+            # run the API
+            c = cdsapi.Client()
+            c.retrieve(
+                kind,
+                {
+                    'product_type': product_type,
+                    'format': 'grib',
+                    'variable': var,
+                    'pressure_level': level,
+                    'year': year,
+                    'month': months,
+                    'day': day,
+                    'time': time,
+                    'grid': [ gridapi, gridapi ]
+                },
+                outfile)
 
 # define propertes for vertical levels
 def define_level(levelout) : 
@@ -71,13 +96,32 @@ def define_time(freq) :
         day = ['01']
         product_type = 'monthly_averaged_reanalysis'
         time_kind = '-monthly-means'
+        minimum_steps = 12
     else : 
         product_type = 'reanalysis'
         time_kind = ''
         day = [str(i).zfill(2) for i in range(1,31+1)]
         if freq == '6hrs' :
             time = [str(i).zfill(2)+':00' for i in range(0,24,6)]
+            minimum_steps = 365*4
         elif freq == '1hr' :
             time = [str(i).zfill(2)+':00' for i in range(0,24)]
+            minimum_steps = 365*12
 
-    return product_type, day, time, time_kind
+    return product_type, day, time, time_kind, minimum_steps
+
+# create filename function
+def create_filename(var, freq, grid, levelout, year) :
+     filename = 'ERA5_' + var + '_' + freq + '_' + grid + '_' + levelout + '_' + year
+     return(filename)
+
+# wrapper for simple parallel function for conversion to netcdf
+def year_convert(infile, outfile) :
+    cdo.copy(input = infile, output = outfile, options = '-f nc4 -z zip')
+
+# get the first and last year from files of a given folder
+def first_last_year(filepattern) :
+    filelist = glob.glob(filepattern)
+    first_year=str(sorted(filelist)[0].split('_')[-1].split('.')[0])
+    last_year=str(sorted(filelist)[-1].split('_')[-1].split('.')[0])
+    return first_year, last_year
--- ERA5_retrieve_postproc.py
+++ ERA5_retrieve_postproc.py
@@ -13,28 +13,39 @@ import os
 from pathlib import Path
 from cdo import Cdo
 from multiprocessing import Process
-from CDS_retriever import year_retrieve
+import glob
+from CDS_retriever import year_retrieve, year_convert, create_filename, first_last_year
+cdo=Cdo()
+
  
+######## -----   USER CONFIGURATION ------- ########
+
  # where this is downloaded
 tmpdir = '/work/scratch/users/paolo/era5'
 
 # where data is stored
-storedir = 'work/datasets/obs/ERA5'
+#storedir = '/work/datasets/obs/ERA5'
+storedir = '/work/scratch/users/paolo/ERA5'
 
-# the variable you want to retrieve
+# the variable you want to retrieve 
 var = 'geopotential'
+
+# the years you need to retrieve
 year1 = 1990
-year2 = 1999
+year2 = 1991
 
 # parallel process
-nprocs = 10
+nprocs = 2
 
 #### - Frequency ---  ####
-freq='mon'
-#freq='6hrs'
+# three different options, monthly get monthly means. 
+#freq='mon'
+freq='6hrs'
 #freq='1hr'
 
 ##### - Vertical levels ---- ####
+# multiple options for surface levels and for pressure levels
+
 # for surface vars
 #levelout = 'sfc' 
 
@@ -51,49 +62,84 @@ grid = '2.5x2.5'
 
 #### - control for the structure --- ###
 do_retrieve = True # retrieve data from CDS
-do_postproc = False # postproc data with CDO
+do_postproc = True # postproc data with CDO
 
+######## ----- END OF USER CONFIGURATION ------- ########
 
+# create list of years
+years = [str(i) for i in range(year1,year2+1)]
+#years = ['1990', '1991', '1992']
+
+# define the out dir and file 
+savedir =  os.path.join(tmpdir, var)
+Path(savedir).mkdir(parents=True, exist_ok=True)
+
+# retrieve block
 if do_retrieve: 
-
-    # define the out dir and file 
-    savedir =  os.path.join(outdir, var)
-    Path(outdir).mkdir(parents=True, exist_ok=True)
-
-    years = [str(i) for i in range(year1,year2+1)]
-    #years = ['1990', '1991', '1992']
-
-    # loop on vars and years
-    #for year in years : 
-    #    year_retrieve(var, freq, year, grid, levelout, outdir)
 
     # loop on the years create the parallel process
     processes = []
     yearlist = [years[i:i + nprocs] for i in range(0, len(years), nprocs)]
     for lyears in yearlist:
-        print(lyears)
         for year in lyears : 
             print(year)
-            p = Process(target=year_retrieve, args=(var, freq, year, grid, levelout, tmpdir))
+            p = Process(target=year_retrieve, args=(var, freq, year, grid, levelout, savedir))
             p.start()
             processes.append(p)
 
-        # wait for all the processes
+        # wait for all the processes to end
         for process in processes:
             process.join()
-   
 
-
+#  
 if do_postproc :
 
     destdir = os.path.join(storedir, var, freq)
     Path(destdir).mkdir(parents=True, exist_ok=True)
 
-    for year in years :
-        filename = 'ERA5_' + var + '_' + freq + '_' + grid + '_' + levelout + '_' + year
-        infile = os.path.join(outdir, destdir + '.grib')
-        outfile = os.path.join(destdir, filename + '.nc')
-        Cdo.copy(infile, outfile, options = '-f nc4 -z zip')
+    # loop on the years create the parallel process for a fast conversion
+    processes = []
+    yearlist = [years[i:i + nprocs] for i in range(0, len(years), nprocs)]
+    for lyears in yearlist:
+        for year in lyears : 
+            print(year)
+            filename = create_filename(var, freq, grid, levelout, year)
+            infile = os.path.join(savedir, filename + '.grib')
+            outfile = os.path.join(destdir, filename + '.nc')
+            p = Process(target=year_convert, args=(infile, outfile))
+            #p = Process(target=cdo.copy, args=(infile, outfile, '-f nc4 -z zip'))
+            p.start()
+            processes.append(p)
 
-  
+        # wait for all the processes to end
+        for process in processes:
+            process.join()
 
+    # extra processing for monthly data
+    if freq == "mon" : 
+        filepattern = os.path.join(destdir, create_filename(var, freq, grid, levelout, '????') + '.nc')
+        first_year, last_year = first_last_year(filepattern)
+        mergefile = os.path.join(destdir, create_filename(var, freq, grid, levelout, first_year + '-' + last_year) + '.nc')
+        if os.path.exists(mergefile):
+            os.remove(mergefile)
+        cdo.cat(input = filepattern, output = mergefile)
+        for f in glob.glob(filepattern): 
+            os.remove(f)
+
+    # extra processing for daily data
+    else : 
+        daydir = os.path.join(storedir, var, 'day')
+        Path(daydir).mkdir(parents=True, exist_ok=True)
+        mondir = os.path.join(storedir, var, 'mon')
+        Path(mondir).mkdir(parents=True, exist_ok=True)
+
+        filepattern = os.path.join(destdir, create_filename(var, freq, grid, levelout, '????') + '.nc')
+        first_year, last_year = first_last_year(filepattern)
+
+        dayfile = os.path.join(daydir, create_filename(var, 'day', grid, levelout, first_year + '-' + last_year) + '.nc')
+        monfile = os.path.join(mondir, create_filename(var, 'mon', grid, levelout, first_year + '-' + last_year) + '.nc')
+
+        if os.path.exists(dayfile):
+            os.remove(dayfile)
+        cdo.daymean(input = '-cat ' + filepattern, outfile = dayfile, options = '-f nc4 -z zip')
+        cdo.monmean(input = dayfile, output = monfile, options = '-f nc4 -z zip')
